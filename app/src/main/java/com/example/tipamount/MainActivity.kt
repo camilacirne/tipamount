a5be1525f41cb59acb59fcf51f17ea2c200bd86b
camilacirne <camilacirne@github.com>
2025-09-11 15:20:38 -0300

    Keyboard button

--- app/src/main/java/com/example/tipamount/MainActivity.kt
+++ app/src/main/java/com/example/tipamount/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -61,13 +62,13 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String{
 }
 
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit, text: String, modifier: Modifier = Modifier) {
+fun EditNumberField(value: String, onValueChange: (String) -> Unit, text: String, keyboardOptions: KeyboardOptions, modifier: Modifier = Modifier) {
     TextField(
         value = value,
         onValueChange = onValueChange,
         label = { Text(text) },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = modifier,
         colors = TextFieldDefaults.colors(
             focusedTextColor = Color.Red,
@@ -84,7 +85,10 @@ fun EditNumberField(value: String, onValueChange: (String) -> Unit, text: String
 fun TipAmountLayout(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0 //converter String em Double, elvis retorna um valor quando a variavel for nula
-    val tip = calculateTip(amount)
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent )
+
 
     Column(
         modifier = modifier
@@ -107,16 +111,29 @@ fun TipAmountLayout(modifier: Modifier = Modifier) {
             Row(modifier = modifier
                 .padding(top = 30.dp)) {
                 EditNumberField(
-                    value = amountInput,
-                    onValueChange = { amountInput = it },
+                    value = tipInput,
+                    onValueChange = { tipInput = it },
                     text = "Bill Amount",
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
                     modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp))
             }
             Spacer(Modifier.size(32.dp))
             Row(modifier = modifier
-                .padding(top = 120.dp)) {
+                .padding(top = 100.dp)) {
+                EditNumberField(
+                    value = amountInput,
+                    onValueChange = { amountInput = it },
+                    text = "Tip Percentage",
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp))
+            }
+            Spacer(Modifier.size(32.dp))
+            Row(modifier = modifier
+                .padding(top = 180.dp)) {
                 Text(
                     text = "Tip amount: $tip",
                     style =  MaterialTheme.typography.displaySmall,
